5332197362669209ea03ad3ceb9cd049afe6f427
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-13 22:55:58 +0200

    new $roles array has been created with role values

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Support\RoleEnum;
 
 class RoleSeeder extends Seeder
 {
@@ -13,6 +15,15 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $roles = [
+          ['title' => 'ROOT', 'description' => 'L\'utente che tutto vede e tutto fa'],
+          ['title' => 'ADMIN', 'description' => 'L\'utente da pannello di controllo'],
+          ['title' => 'RESTAURANT', 'description' => 'Utente ristoratore'],
+          ['title' => 'GUEST', 'description' => 'L\'utente del sito web'],
+        ];
+
+        foreach ($roles as $role) {
+          $role = Role::firstOrCreate($role);
+        }
     }
 }
