17952d653cbf2fbb20a08469d082a86f2c4d74fd
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-13 22:48:00 +0200

    string param changed from name to title value

--- database/migrations/2021_06_11_152939_create_roles_table.php
+++ database/migrations/2021_06_11_152939_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
             $table->text('description')->nullable();
             $table->timestamps();
             $table->softDeletes();
