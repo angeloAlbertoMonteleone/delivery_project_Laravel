19066281a544a63ddaba0cfc25acfce243d2514c
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-13 22:42:47 +0200

    new credentials added to casts, fillable, attributes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,63 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
+use NumberFormatter;
 
+/**
+ * App\Models\Product
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Product whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $attributes = [
+      'visible' => false
+    ];
+
+
+    protected $fillable = [
+      'title',
+      'description',
+      'slug',
+      'visible',
+      'price'
+    ];
+
+
+    protected $casts = [
+      'visible' => 'boolean',
+      'price' => 'decimal:2'
+    ];
+
+
+    //Defining an Accessor - if I have to show the product in some way
+    public function getPriceAttribute(float $value):string
+    {
+      $ftm = new NumberFormatter('it_it', NumberFormatter::CURRENCY);
+
+      return $ftm->formatCurrency($value, 'EUR');
+    }
+
+    // Defining A Mutator - when I have to set up some rules
+    public function setPriceAttribute($value)
+    {
+      if($value < 0) {
+        $value = $value * -1;
+      }
+
+      $this->attributes['price'] = round($value, 2);
+    }
+
 }
