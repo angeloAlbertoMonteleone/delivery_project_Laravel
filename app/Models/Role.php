cb1bd136daa3d2bc87ceb4a7b836b4a87d292e2d
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-13 22:47:05 +0200

    new attributes added in fillable, attributes, casts var

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -3,21 +3,44 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\softDeletes;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Role
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property \datetime|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role newQuery()
+ * @method static \Illuminate\Database\Query\Builder|Role onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Role whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Role withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Role withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
 
     // const CREATED_AT = 'creation_date';
     // const UPDATED_AT = 'updated_date';
 
     protected $attributes = [
-      'title' => 'Nuovo ruolo'
     ];
 
     protected $fillable = [
-      'name',
+      'title',
       'description'
     ];
 
@@ -27,11 +50,14 @@ class Role extends Model
     ];
 
     protected $visible = [
-
+      'title',
+      'description',
+      'created_at',
+      'deleted_at'
     ];
 
     protected $casts = [
-
+      'created_at' => 'datetime:d/m/Y'
     ];
 
 
