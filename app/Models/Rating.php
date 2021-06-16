31917ba0eaff3b30fa5932f056052bb6203cefb8
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-16 21:52:07 +0200

    new function restaurant to create relation with restaurant class

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -5,7 +5,40 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Backoffice\Restaurant;
 
+/**
+ * App\Models\Rating
+ *
+ * @property int $id
+ * @property int $restaurant_id
+ * @property int $rating
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
+ * @property-read Restaurant $restaurant
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating bad()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating currentMonthVeryBad()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating good()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating notTooBad()
+ * @method static \Illuminate\Database\Query\Builder|Rating onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating veryBad()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating veryGood()
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereRating($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereRestaurantId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Rating whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|Rating withTrashed()
+ * @method static \Illuminate\Database\Query\Builder|Rating withoutTrashed()
+ * @mixin \Eloquent
+ */
 class Rating extends Model
 {
     use HasFactory,
@@ -16,6 +49,9 @@ class Rating extends Model
     ];
 
 
+    public function restaurant(){
+      return $this->belongsTo(Restaurant::class);
+    }
 
     /**
     * Scope a query to only include popular users.
