853069d11c72c960016006f7625c8853eda7c89c
angeloAlberto <angeloalberto.monteleone@gmail.com>
2021-06-17 22:32:46 +0200

    new $categories array has been created

--- database/seeders/ProductCategorySeeder.php
+++ database/seeders/ProductCategorySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Backoffice\ProductCategory;
 
 class ProductCategorySeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class ProductCategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories = [
+              ['title' => 'Pizze', 'description' => 'Lorem ipsum dolor sit amet.', 'slug' => Str::slug('Pizze'), 'visible' => true],
+          ];
+
+        foreach ($categories as $category) {
+          $category = ProductCategory::create($category);
+        }
     }
 }
